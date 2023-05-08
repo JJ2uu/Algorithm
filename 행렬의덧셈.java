4e273d069eee30184e1b891731ef2628f615c160
JJuu <jyj120572@gmail.com>
2023-05-08 20:03:37 +0900

    프로그래머스 1단계 - 행렬의 덧셈

---
+++ 행렬의덧셈.java
@@ -0,0 +1,29 @@
+package 프로그래머스;
+
+public class 행렬의덧셈 {
+
+	public static void main(String[] args) {
+		int[][] arr1 = {{1}, {2}};
+		int[][] arr2 = {{3}, {4}};
+
+							// arr1의 행의 개수   arr1[0] 열의 개수
+		int[][] answer = new int[arr1.length][arr1[0].length];
+
+		
+		for (int i = 0; i < arr1.length; i++) { // 행마다
+			for (int j = 0; j < arr1[i].length; j++) { //열마다
+				answer[i][j] = arr1[i][j] + arr2[i][j];
+			}
+		}
+		
+		answer = arr1.clone();
+		
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr1[i].length; j++) {
+				answer[i][j] += arr2[i][j]; // answer에 더함
+			}
+		}
+
+	}
+
+}
