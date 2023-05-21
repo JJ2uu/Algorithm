b040a2c8ae6d2150d14175c3d3194982e36aebe9
JJuu <jyj120572@gmail.com>
2023-05-21 19:50:40 +0900

    프로그래머스 1단계

--- 소수만들기.java
+++ 소수만들기.java
@@ -13,16 +13,29 @@ public class 소수만들기 {
 
 class Solution14 {
     public int solution(int[] nums) {
-        int answer = -1;
+        int answer = 0;
         
         for (int i = 0; i < nums.length; i++) {
-			for (int j = 0; j < nums.length; j++) {
-				for (int k = 0; k < nums.length; k++) {
+			for (int j = i+1; j < nums.length; j++) {
+				for (int k = j+1; k < nums.length; k++) {
 					int sum = nums[i] + nums[j] + nums[k];
+					if (number(sum)) {
+						answer++;
+					}
 				}
 			}
 		}
 
         return answer;
     }
+    
+    public boolean number(int sum) {
+    	boolean check = true;
+    	for (int l = 2; l < sum; l++) {
+			if (sum % l == 0) {
+				check = false;
+			}
+		}
+    	return check;
+	}
 }
