27d3bfec42f3acd7b5d250e6b265abf6776168fa
JJ2uu <124110688+JJ2uu@users.noreply.github.com>
2023-06-21 19:50:32 +0900

    2022 KAKAO BLIND RECRUITMENT

--- 프로그래머스/2단계/K진수에서 소수 개수 구하기/K진수에서소수개수구하기.java
+++ 프로그래머스/2단계/K진수에서 소수 개수 구하기/K진수에서소수개수구하기.java
@@ -14,43 +14,43 @@ public class K진수에서소수개수구하기 {
 
 class Solution23 {
     public int solution(int n, int k) {
-        int answer = 0;
-        String res = kNum(n, k);
-        String[] arr = res.split("0");
-        for (int i = 0; i < arr.length; i++) {
-			if (!arr[i].isEmpty()) {
-				if (isPrime(Long.parseLong(arr[i]))) {
-					answer++;
-				}
+	int answer = 0;
+	String res = kNum(n, k);
+	String[] arr = res.split("0");
+	for (int i = 0; i < arr.length; i++) {
+		if (!arr[i].isEmpty()) {
+			if (isPrime(Long.parseLong(arr[i]))) {
+				answer++;
 			}
 		}
-        return answer;
+	}
+	return answer;
     }
     
     // K진수 변환 메서드
     public String kNum(int n, int k) {
-		String s = "";
-		while (n > 0) {
-			s = n%k +s;
-			n /= k;
-		}
-		return s;
+	String s = "";
+	while (n > 0) {
+		s = n%k +s;
+		n /= k;
 	}
+	return s;
+    }
     
     // 소수 확인 메서드
     public boolean isPrime(long n) {
     	boolean check = true;
     	if (n == 1) {
-			check = false;
-		}
+		check = false;
+	}
     	if (n == 2) {
     		check = true;
     	}
     	for (int i = 2; i <= (int)Math.sqrt(n); i++) {
-			if (n % i == 0) {
-				check = false;
-			}
+		if (n % i == 0) {
+			check = false;
 		}
-    	return check;
 	}
+    	return check;
+    }
 }
